80c94e44b00d78a31a9cd5744f24f7e4c6db53ab
José Nigério Evangelista Lima Bezerra <nigerio@nimolabs.net>
2021-10-03 19:26:02 +0200

    tentativas de fixar display.

--- src/app/difficulty/difficulty.component.ts
+++ src/app/difficulty/difficulty.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: 'difficulty-component',
     template: 
-       `<h4>Dificuldade</h4>
-        <select class="form-control" [attr.disabled]="disabled" [(ngModel)]="selectedDifficulty" (ngModelChange)="onChange($event)">
+       `<h6>Dificuldade</h6>
+        <select class="form-control-sm" [attr.disabled]="disabled" [(ngModel)]="selectedDifficulty" (ngModelChange)="onChange($event)">
            <option *ngFor="let c of difficulties" value="{{c.id}}">{{c.name}}</option>
         </select>`
      })
