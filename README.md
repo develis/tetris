5b121c9c5ab690abcd773be83e540a40e7af4f61
Elis Marcela <82593291+develis@users.noreply.github.com>
2021-10-03 18:43:02 -0300

    adjusting mobile functions

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tetris feito em Angular
+📍 https://develis.github.io/tetris/ <br>
+📍 Reproduzido por: Elis Marcela, Margareth Reis, Natali Lucas, Nigério Bezerra, Pamela Oliveira. <br>
+📍 Baseado no artigo publicado no site Medium produzido por <a href="https://twitter.com/melcor76">Michael Karén</a>. <br>
+
+## 🚀 Implementações: 
+✔ Seletor de dificuldade <br>
+✔ Efeitos sonoros de movimentação e posicionamento das peças <br>
+✔ Alteração no sistema de pontuação <br>
+✔ Estilização do tabuleiro e das peças <br>
+✔ Mobile-friendly
--- src/app/difficulty/difficulty.component.ts
+++ src/app/difficulty/difficulty.component.ts
@@ -1,36 +1,36 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-    selector: 'difficulty-component',
-    template: 
-       `<h6>Dificuldade</h6>
+   selector: 'difficulty-component',
+   template:
+      `<h6>Dificuldade</h6>
         <select class="form-control-sm" [attr.disabled]="disabled" [(ngModel)]="selectedDifficulty" (ngModelChange)="onChange($event)">
            <option *ngFor="let c of difficulties" value="{{c.id}}">{{c.name}}</option>
         </select>`
-     })
- export class DifficultyComponent implements OnInit{
-    ngOnInit(): void {
-        this.selectedDifficulty = 1;
-    }
-    difficulties = [
-       {id: 1, name: "Fácil"},
-       {id: 3, name: "Médio"},
-       {id: 6, name: "Difícil"},
-    ];
-    selectedDifficulty: number = 1;
-    disabled: string = null;
+})
+export class DifficultyComponent implements OnInit {
+   ngOnInit(): void {
+      this.selectedDifficulty = 1;
+   }
+   difficulties = [
+      { id: 1, name: "Fácil" },
+      { id: 3, name: "Médio" },
+      { id: 6, name: "Difícil" },
+   ];
+   selectedDifficulty: number = 1;
+   disabled: string = null;
 
-    onChange(newValue:any): void {
-        console.log(newValue)
-    }
+   onChange(newValue: any): void {
+      console.log(newValue)
+   }
 
-    notifyGameStatus(status: boolean) : void {
-       if(status){
+   notifyGameStatus(status: boolean): void {
+      if (status) {
          this.disabled = 'disabled';
-       }
-       else {
+      }
+      else {
          this.disabled = null;
-       }
-    }
+      }
+   }
 
- }
+}
